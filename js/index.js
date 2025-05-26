1c2655170cbadc109238c5528cb982682559134e
Siarhei Melnikau <melnikaus@gmail.com>
2025-05-26 14:56:26 +0200

    Added folders for css and js code. Added a post page and a common page with posts

--- js/index.js
+++ js/index.js
@@ -1,43 +1,3 @@
-const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
-
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
-  }
-
-}
-
-const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
-
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
-
-window.addEventListener('keydown', handleFirstTab)
-
-const backToTopButton = document.querySelector(".back-to-top");
-let isBackToTopRendered = false;
-
-let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
-};
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
-    alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
-});
-
 let technologyStack1 = ["AEM 6.5+","AEM as Cloud service","Java","Apache Sling","OSGI (Apache Felix)","Apache Jackrabbit OAK","HTL(Sightly)","Sling Models","jQuery","JavaScript","GIT","Maven"];
 
 let technologyStack2 = ["AEM 5.6 - AEM 6.5+","Java","Java EE","Apache Sling","OSGI (Apache Felix)","Apache Jackrabbit OAK","HTL(Sightly)","Sling Models","jQuery","JavaScript","GIT","Maven","Groovy","AEM Forms","AWS Lambda","Amazon Cognito","Ext JS"];
@@ -89,7 +49,5 @@ var typedTechnologyStack4 = new Typed('.technology-stack4-update', technologySta
 
 
 var typedSkills = new Typed('.skill-update', skillsTypedConfig);
-
-document.getElementById("year").innerHTML = new Date().getFullYear();
 
 document.getElementById("experience").innerHTML = new Date().getFullYear() - 2016;
